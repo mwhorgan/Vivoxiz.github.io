15b7f0d1331ac3625bcd46e237b606791e6baba3
Vivoxiz <isobonja@gmail.com>
2016-05-17 09:01:41 -0400

    Update StartMenu.js

--- Inverted Racer/StartMenu.js
+++ Inverted Racer/StartMenu.js
@@ -19,10 +19,10 @@ invertedRacer.StartMenu.prototype = {
         this.startText.inputEnabled = true;
         this.startText.events.onInputDown.addOnce(this.startGame, this);
         
-        this.instruct = this.add.bitmapText(150, this.world.centerY + 100, 'VCR_OSD', 'INSTRUCTIONS', 54);
-        this.instruct.tint = 0xF9891C;
-        this.instruct.inputEnabled = true;
-        this.instruct.events.onInputDown.addOnce(this.howToPlay, this);
+//        this.instruct = this.add.bitmapText(150, this.world.centerY + 100, 'VCR_OSD', 'INSTRUCTIONS', 54);
+ //       this.instruct.tint = 0xF9891C;
+ //       this.instruct.inputEnabled = true;
+ //       this.instruct.events.onInputDown.addOnce(this.howToPlay, this);
     },
     
     startGame: function (pointer) {
@@ -38,12 +38,12 @@ invertedRacer.StartMenu.prototype = {
         van = true;
     },
     
-    howToPlay: function (pointer) {
-        this.startText.destroy();
-        this.instruct.destroy();
-        this.startBG.destroy();
-        this.vanEasterEgg.destroy();
-        this.startText.destroy();
-        this.state.start('Instructions');
-    },
+//    howToPlay: function (pointer) {
+ //       this.startText.destroy();
+ //       this.instruct.destroy();
+ //       this.startBG.destroy();
+ //       this.vanEasterEgg.destroy();
+ //       this.startText.destroy();
+ //       this.state.start('Instructions');
+ //   },
 };
