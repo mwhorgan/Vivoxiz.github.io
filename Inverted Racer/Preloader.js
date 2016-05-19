1c8b92ccb30fa479ec98f19927f24fed26bfa6f9
Vivoxiz <isobonja@gmail.com>
2016-05-19 09:38:07 -0400

    More bush stuff

--- Inverted Racer/Preloader.js
+++ Inverted Racer/Preloader.js
@@ -10,7 +10,7 @@ invertedRacer.Preloader.prototype = {
         this.load.bitmapFont('eightbitwonder', 'fonts/eightbitwonder.png', 'fonts/eightbitwonder.fnt');
         this.load.bitmapFont('Ipixelu', 'fonts/Ipixelu/Ipixelu.png', 'fonts/Ipixelu/Ipixelu.fnt');
         this.load.bitmapFont('VCR_OSD', 'fonts/VCR_OSD_MONO_1.001/VCR_OSD_MONO_1.001.png', 'fonts/VCR_OSD_MONO_1.001/VCR_OSD_MONO_1.001.fnt');
-
+         
         this.load.image('car1', 'assets/car1.png');
         this.load.image('enemy1.1', 'assets/enemy1.1.png');
         this.load.image('enemy2.1', 'assets/enemy2.1.png');
@@ -24,6 +24,7 @@ invertedRacer.Preloader.prototype = {
         this.load.image('police', 'assets/Police.png');
         this.load.image('taxi', 'assets/taxi.png');
         this.load.image('cone', 'assets/traffic cone.png');
+        this.load.image('bush', 'assets/bush.png');
 
         this.load.image('boom1', 'assets/explosion1.fw.png');
     },
