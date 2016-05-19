0bf5af4f6b5edaa66bd3fa5033dd4ea520f0c770
Vivoxiz <isobonja@gmail.com>
2016-05-19 09:41:29 -0400

    Bush stuff

--- Inverted Racer/Boot.js
+++ Inverted Racer/Boot.js
@@ -7,6 +7,12 @@ invertedRacer.Boot.prototype = {
         this.load.image('titlescreen', 'assets/titlescreen.png');
         this.load.image('startGameText', 'assets/startGameText.png');
         this.load.image('candyVan', 'assets/Candy_van.png');
+        
+        this.load.image('wKey', 'assets/w key.png');
+        this.load.image('sKey', 'assets/s key.png');
+        this.load.image('dKey', 'assets/d key.png');
+        this.load.image('aKey', 'assets/a key.png');
+
     },
     
     create: function() {
