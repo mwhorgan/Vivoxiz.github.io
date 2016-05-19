d06da6b2831e4b77a39fea057b2956a553f1bc26
Vivoxiz <isobonja@gmail.com>
2016-05-19 09:48:26 -0400

    Update StartMenu.js

--- Inverted Racer/StartMenu.js
+++ Inverted Racer/StartMenu.js
@@ -18,30 +18,17 @@ invertedRacer.StartMenu.prototype = {
         this.startText = this.add.image(110, this.world.centerY, 'startGameText');
         this.startText.inputEnabled = true;
         this.startText.events.onInputDown.addOnce(this.startGame, this);
-        
-        this.instruct = this.add.bitmapText(150, this.world.centerY + 100, 'VCR_OSD', 'INSTRUCTIONS', 54);
-        this.instruct.tint = 0xF9891C;
-        this.instruct.inputEnabled = true;
-        this.instruct.events.onInputDown.addOnce(this.howToPlay, this);
     },
     
     startGame: function (pointer) {
         this.state.start('Game');
         this.startBG.destroy();
         this.startText.destroy();
+        this.vanEasterEgg.destroy();
     },
     
     playerVan: function (pointer) {
         this.vanEasterEgg.destroy();
         van = true;
-    },
-    
-    howToPlay: function (pointer) {
-        this.startText.destroy();
-        this.instruct.destroy();
-        this.startBG.destroy();
-        this.vanEasterEgg.destroy();
-        this.startText.destroy();
-        this.state.start('Instructions');
     },
 };
